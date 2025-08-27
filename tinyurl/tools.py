a97183cc9d8ab456ef6471b89c9046ccec0274a1
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2025-08-27 19:21:20 +0000

    minor temp changes

--- tinyurl/tools.py
+++ tinyurl/tools.py
@@ -1,19 +1,21 @@
+from typing import Any
+
 from server import mcp
-from models import ShortUrlRequest, ShortUrlResponse
+from models import ShortUrlRequest
 from utils import make_api_request
 
 
 @mcp.tool()
-def generate_short_url(req_data: ShortUrlRequest) -> ShortUrlResponse:
+async def generate_short_url(req_data: ShortUrlRequest) -> Any:
     """
     Generate a short URL for a given long URL.
 
     Args:
-        req_data: The long URL to shorten and additional optional parameters.
+        req_data: The long URL to shorten along with additional optional parameters.
 
     Returns:
         A short URL for the given long URL if success else error code along with the message.
     """
     api_response = await make_api_request(api_path="create", req_data=req_data.model_dump())
 
-    return ShortUrlResponse(api_response)
+    return api_response
