d40884e1a67ce15c8a352052989195bef60e66a7
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2025-08-28 19:26:36 +0000

    add prompt skeleton

--- tinyurl/prompts.py
+++ tinyurl/prompts.py
@@ -2,10 +2,12 @@ from server import mcp
 
 
 @mcp.prompt()
-def generate_short_url() -> str:
+def generate_short_url_prompt() -> str:
     """
     Prompt to use while generating a short URL.
-    :return: The prompt string
+
+    Returns:
+        The prompt string
     """
     return """
     """
