0010676156abfbb2299aa1beabc0b449bdfde9ba
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2025-09-07 15:18:43 +0000

    add additional models

--- rebrandly/src/rebrandly_mcp/models.py
+++ rebrandly/src/rebrandly_mcp/models.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl, PositiveInt
 from typing import Optional
 
 
 class CustomDomain(BaseModel):
-    id: str = Field(description="The unique identifier for the custom domain.")
+    id: Optional[str] = Field(description="The unique identifier for the custom domain.")
+    fullName: Optional[str] = Field(description="The fully qualified custom domain name.")
 
 
 class CreateShortUrlRequest(BaseModel):
@@ -35,7 +36,7 @@ class CreateShortUrlRequest(BaseModel):
     )
 
 
-class ShortUrlInfo(BaseModel):
+class ShortUrlDetails(BaseModel):
     """
     Response schema containing the short URL and metadata.
     """
@@ -46,9 +47,42 @@ class ShortUrlInfo(BaseModel):
     shortUrl: str = Field(description="The short link pointing to the long/destination URL")
 
 
-class ShortUrlInfoList(BaseModel):
+class ShortUrlDetailsList(BaseModel):
     """
     Response schema containing the short URL and metadata.
     """
 
-    urls: list[ShortUrlInfo] = Field(description="List of short URLs with details.")
+    urls: list[ShortUrlDetails] = Field(description="List of short URLs with details.")
+
+
+class GetOrListShortUrlsRequest(BaseModel):
+    """
+    Request schema for getting or listing short URLs.
+    """
+
+    limit: PositiveInt = Field(
+        default=25, description="The maximum number of short URLs to retrieve.", max=25
+    )
+    domain: Optional[CustomDomain] = Field(
+        default=None, description="The unique id of the domain to use for the short URL."
+    )
+    slashtag: Optional[str] = Field(
+        default=None,
+        description="The slashtag or the custom identifier for the short URL. Custom domain is required to use this field",
+    )
+    dateFrom: Optional[str] = Field(
+        default=None,
+        description="The start date to filter short URLs by creation date. Date format must be YYYY-MM-DD",
+    )
+    dateTo: Optional[str] = Field(
+        default=None,
+        description="The end date to filter short URLs by creation date. Date format must be YYYY-MM-DD",
+    )
+
+
+class DeleteShortUrlRequest(BaseModel):
+    """
+    Request schema for deleting a short URL.
+    """
+
+    id: str = Field(description="The unique identifier of the short URL that you want to delete")
